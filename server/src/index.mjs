a11ac18f2cd25db687f332e2b818b512b3afc409
Abed25 <officialwekesa254@gmail.com>
2024-11-19 10:18:49 +0300

    cors middleware in effect

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes/index.mjs";
 
 const app = express();
@@ -6,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json());
+app.use(cors());
 
 app.use(routes);
 
